2e86e359ddc7bf87d174eb6a610a940f8659171a
Gigih Hadi P <hadigigih@gmail.com>
2020-01-24 09:02:25 +0700

    first commit

---
+++ src/_body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Body(){
+    return (
+        <section class="hero is-light is-large">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">Primary bold title</h1>
+                    <h2 class="subtitle">Primary bold subtitle</h2>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default Body;
