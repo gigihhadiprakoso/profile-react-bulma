926d415081fcaf66f011f7f780afade03122f770
Gigih Hadi P <hadigigih@gmail.com>
2020-03-10 21:44:08 +0700

    [ADD] perubahan pada header

--- src/_body.js
+++ src/_body.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 function Body(){
     return (
-        <section class="hero is-light is-large">
+        <section class="hero is-primary is-large">
             <div class="hero-body">
                 <div class="container">
-                    <h1 class="title">Primary bold title</h1>
-                    <h2 class="subtitle">Primary bold subtitle</h2>
+                    {/* <h1 class="title">Primary bold title</h1>
+                    <h2 class="subtitle">Primary bold subtitle</h2> */}
+                    <figure class="image is-128x128" style={{marginRight:'auto',marginLeft:'auto'}}>
+                        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" />
+                    </figure>
                 </div>
             </div>
         </section>
